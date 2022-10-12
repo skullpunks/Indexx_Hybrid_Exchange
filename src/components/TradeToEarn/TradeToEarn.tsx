5b5a49b9aa9547af8898ec6d1df563f6608b7af7
Kamal Farooqi <76092669+kamalaqeel@users.noreply.github.com>
2022-10-12 13:13:39 +0500

    fixed minor issues and made build ready

--- src/components/TradeToEarn/TradeToEarn.tsx
+++ src/components/TradeToEarn/TradeToEarn.tsx
@@ -1,7 +1,6 @@
-import { Button ,Card, Image } from 'antd'
-import { Divider,Space } from 'antd';
+import { Card, Image, Button } from 'antd'
+import { Divider} from 'antd';
 import { Typography } from 'antd';
-
 import React from 'react'
 import exgcoin from  "../../assets/arts/exgcoin.png";
 import exglady from "../../assets/arts/exglady.png";
@@ -11,7 +10,7 @@ import no3 from "../../assets/arts/no3.png";
 
 import Footer from '../Footer/Footer';
 
-const { Text , Title} = Typography;
+const { Text } = Typography;
 const TradeToEarn = () => {
   return (
     <>
@@ -29,23 +28,23 @@ const TradeToEarn = () => {
         justifyContent: 'center',
       
       }}>
-          <Image src={exgcoin} width={120}></Image>
+          <Image src={exgcoin} width={120} preview={false}></Image>
         </div>
         <h2 className='centered' style={{ marginBottom: 0, color: "#5F5F5F", fontSize: "25px" }}>indexx Exchange (INEX)</h2>
         <h1 style={{display:'flex',marginTop:0,justifyContent:'center',alignItems:'center',opacity:"20%", color: "#5F5F5F", fontSize: "90px"}}>$0</h1>
        <Text className='centered' style={{ marginBottom: 0, color: "#5F5F5F", fontSize: "15px" }}>Minimum limit:100</Text>
         <br /> <br></br>
-        <button   style={{ backgroundColor:"#0084ff",width: "371px",
+        <Button type="primary"  block shape="round" size="large" className="btn_xl"  style={{ width: "371px",
             height: "55px",
-            borderRadius: "5px",textAlign:"center",fontSize: "22px"}}>Withdraw Tokens</button>
+            borderRadius: "5px"}}>Withdraw Tokens</Button>
         
           
         </Card>
         
-        <Image src={exglady} style={{display:'flex',justifyContent:'center',width:650,alignItems:'center'}}></Image> 
-        <Image src={no1} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image> 
-        <Image src={no2} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image>  
-        <Image src={no3} style={{paddingLeft:10,display:'flex',justifyContent:'center',width:480,alignItems:'center'}}></Image> 
+        <Image preview={false} src={exglady} style={{display:'flex',justifyContent:'center',width:650,alignItems:'center'}}></Image> 
+        <Image preview={false} src={no1} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image> 
+        <Image preview={false} src={no2} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image>  
+        <Image preview={false} src={no3} style={{paddingLeft:10,display:'flex',justifyContent:'center',width:480,alignItems:'center'}}></Image> 
         
         </div>
       
