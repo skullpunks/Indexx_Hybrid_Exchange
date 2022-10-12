af47b99ed6a94fa04b4c8d745f22207805e219c2
Sai Kumar Munagala <saikumar.munagala@gmail.com>
2022-10-12 20:09:41 +0530

    indexBlogChanges

--- src/components/IndexBlog/IndexBlog.tsx
+++ src/components/IndexBlog/IndexBlog.tsx
@@ -26,25 +26,25 @@ export const IndexBlog = () => {
                 <div className='d-flex flex-justify-between flex-wrap'>
                     <div className='flex-b-50 border-1x margin-b-2x'>
                         <img src={IndexPreSaleIcon} alt="IndexPreSaleIcon" className='width-100' />
-                        <p className='font_48x padding-lr-2x'>Indexx 500 and Indexx Crypto’s presale is ON!</p>
+                        <p className='font_48x padding-lr-2x padding-t-2x'>Indexx 500 and Indexx Crypto’s presale is ON!</p>
                         <p className='font_30x padding-lr-2x margin-t-2x margin-b-3x '> As per our last announcement, we have some exciting news to share with you all.</p>
                         <span className='font_20x text-center d-block padding-b-1x'>September 13, 2022 - 2 min read</span>
                     </div>
                     <div className='flex-b-50 border-1x  margin-b-2x'>
                         <img src={indexFamilyToken} alt="indexFamilyToken" className='width-100' />
-                        <p className='font_48x padding-lr-2x'>Indexx 500 and Indexx Crypto’s presale is ON!</p>
+                        <p className='font_48x padding-lr-2x padding-t-2x'>Indexx 500 and Indexx Crypto’s presale is ON!</p>
                         <p className='font_30x padding-lr-2x margin-t-2x margin-b-3x ' > As per our last announcement, we have some exciting news to share with you all.</p>
                         <span className='font_20x text-center d-block padding-b-1x'>August 23, 2022 - 2 min read</span>
                     </div>
                     <div  className='flex-b-50 border-1x  margin-tb-2x'>
                     <img src={indexFund} alt="indexFund" className='width-100'/>
-                    <p className='font_48x padding-lr-2x'>Indexx 500 Opens a new way to Index funds</p>
+                    <p className='font_48x padding-lr-2x padding-t-2x'>Indexx 500 Opens a new way to Index funds</p>
                     <p className='font_30x padding-lr-2x margin-t-2x margin-b-3x ' > 02 August 2022 The launch of a token backed by S&P Holdings and as development is moving forward the hunt for investors to buy...</p>
                     <span className='font_20x text-center d-block padding-b-1x'>August 23, 2022 - 2 min read</span>
                 </div>
                 <div  className='flex-b-50 border-1x  margin-tb-2x'>
                     <img src={indexFinance} alt="indexFinance" className='width-100'/>
-                    <p className='font_48x padding-lr-2x'>Indexx, the future of Stock Tokens — An Asset-Backed token by S&P 500 holdings</p>
+                    <p className='font_48x padding-lr-2x padding-t-2x'>Indexx, the future of Stock Tokens — An Asset-Backed token by S&P 500 holdings</p>
                     <p className='font_30x padding-lr-2x margin-t-2x margin-b-3x ' > The Indexx Stock Token is an asset-backed token pegged to the S&P 500.</p>
                     <span className='font_20x text-center d-block padding-b-1x'>July 4, 2022 - 2 min read</span>
                 </div>
