acb19911d5c47efaee078cbd85e240818220f011
Sai Kumar Munagala <talpasaikumar.munagala@gmail.com>
2022-10-14 20:28:56 +0530

    Pushed Wallet changes

--- src/components/BuySell/BSWalletTop.tsx
+++ src/components/BuySell/BSWalletTop.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import openEye from "../../assets/arts/openEye.svg";
 const BSWalletTop = () => {
     return (
-        <div>BSWalletTop</div>
+        <>
+            <div className='border-b-1x orange width-100 padding-t-2x'>
+                <h1 className='padding-l-3x'>Funding</h1>
+            </div>
+            <div className='padding-l-3x padding-t-1x width-100'>
+                <h2>Estimated Balance</h2>
+                <div className='d-flex flex-align-center color_general'>
+                    <h2 className='margin-b-0'>$</h2>
+                    <h1 className='margin-b-0'>11.33</h1>
+                    <img className='padding-l-1x' src={openEye} alt="eye" />
+                </div>
+
+            </div>
+        </>
     )
 }
 
