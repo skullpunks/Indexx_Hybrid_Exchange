fa32539706a9edd609d585b95cc131f059216bcd
Kamal Farooqi <76092669+kamalaqeel@users.noreply.github.com>
2022-10-15 21:38:30 +0500

    changed image

--- src/components/TradeToEarn/TradeToEarn.tsx
+++ src/components/TradeToEarn/TradeToEarn.tsx
@@ -1,17 +1,23 @@
-import { Card, Image, Button, Input} from 'antd'
+import { Card, Image, Button, Input } from 'antd'
 import { Divider} from 'antd';
 import { Typography } from 'antd';
-import React from 'react'
+
 import exgcoin from  "../../assets/arts/exgcoin.png";
 import exglady from "../../assets/arts/exglady.png";
+import exghands from "../../assets/arts/exghands.png";
+
 import no1 from "../../assets/arts/no1.png";
 import no2 from "../../assets/arts/no2.png";
 import no3 from "../../assets/arts/no3.png";
 
 import Footer from '../Footer/Footer';
 
+
 const { Text } = Typography;
+
 const TradeToEarn = () => {
+ 
+
   return (
     <>
     <div className='scan-container trade-to-earn flex-direction-column '>
@@ -34,22 +40,24 @@ const TradeToEarn = () => {
         <h1 style={{display:'flex',marginTop:0,justifyContent:'center',alignItems:'center',opacity:"20%", color: "#5F5F5F", fontSize: "90px"}}>$0</h1>
        <Text className='centered' style={{ marginBottom: 0, color: "#5F5F5F", fontSize: "15px" }}>Minimum limit:100</Text>
         <br /> <br></br>
-        <Button type="primary"  block shape="round" size="large" className="btn_xl"  style={{ width: "371px",
+        <Button type="primary"  block shape="round" size="large" className="btn_xl"  style={{
             height: "55px",
             borderRadius: "5px",
             
             }}>Withdraw Tokens</Button>
         
         <br/><br/>
-        <Input.Group compact>
-        <Input size={"middle"} style={{ width: '100%', marginBottom: "10px"}} defaultValue="Enter Wallet Address" />
-        <Button type="primary" style={{borderRadius: "5px",}}>Submit Wallet Address</Button>
+
+        <Input.Group compact style={{paddingTop: "40px"}}>
+        <Input size={"middle"} style={{  width: '100%', marginBottom: "10px"}} defaultValue="Enter Wallet Address" />
+        <Button type="primary" block shape="round" size="large" className="btn_xl" style={{
+            height: "55px",borderRadius: "5px",}}>Submit Wallet Address</Button>
       </Input.Group>
       <br />
 
         </Card>
         
-        <Image preview={false} src={exglady} style={{display:'flex',justifyContent:'center',width:650,alignItems:'center'}}></Image> 
+        <Image preview={false} src={exghands} style={{paddingTop:100,display:'flex',justifyContent:'center',width:400,alignItems:'center'}}></Image> 
         <Image preview={false} src={no1} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image> 
         <Image preview={false} src={no2} style={{display:'flex',justifyContent:'center',width:500,alignItems:'center'}}></Image>  
         <Image preview={false} src={no3} style={{paddingLeft:10,display:'flex',justifyContent:'center',width:480,alignItems:'center'}}></Image> 
