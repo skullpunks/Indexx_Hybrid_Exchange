9177f725398f8c087205d3c90ec2bcb599569dd3
Sai Kumar Munagala <saikumar.munagala@gmail.com>
2022-10-22 19:42:20 +0530

    INDEXXTokens

--- src/components/IndexxTokens/IndexxTokens.tsx
+++ src/components/IndexxTokens/IndexxTokens.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import INXC from "../../assets/arts/INXC.svg";
+import INEX from "../../assets/arts/INEX.svg";
+import INFX from "../../assets/arts/INFX.svg";
+import IUSD from "../../assets/token-icons/35.png";
+import IN500 from "../../assets/token-icons/33.png";
 
 const IndexxTokens = () => {
     return (
-        <div className='scan-container'>IndexxTokens - In Progress</div>
+        <div className='scan-container d-block'>
+           <h1 className='font_50x margin-t-3x centered'>
+            Prosperous life awaits with indexx tokens
+            </h1>
+            <div className='d-flex flex-justify-between index_tokens row padding-t-3x margin-t-3x'>
+                <div className='text-center d-block col-md-4'>
+                    <img src={INXC} alt="indexicon" />
+                    <div className='font_25x padding-t-2x'>iNXC </div>
+                    <div className='font_15x'>indexx Crypto</div>
+                </div>
+                <div className='col-md-4 text-center d-block'>
+                    <img src={IN500} alt="indexicon" />
+                    <div className='font_25x padding-t-2x'>iN500  </div>
+                    <div className='font_15x'>indexx 500</div>
+                </div>
+                <div className='col-md-4 text-center d-block'>
+                    <img src={IUSD} alt="indexicon" />
+                    <div className='font_25x padding-t-2x'> iUSD+  </div>
+                    <div className='font_15x'>indexx USD+</div>
+                </div>
+                <div className='d-flex row padding-t-3x'>
+                <div className='col-md-6 text-center d-block padding-t-3x'>
+                    <img src={INEX} alt="indexicon" />
+                    <div className='font_25x padding-t-2x'> iNEX  </div>
+                    <div className='font_15x'>indexx Exchange</div>
+                </div>
+                <div className='col-md-6 text-center d-block padding-t-3x'>
+                    <img src={INFX} alt="indexicon" />
+                    <div className='font_25x padding-t-2x'> iNXF  </div>
+                    <div className='font_15x'>indexx Fortune</div>
+                </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
