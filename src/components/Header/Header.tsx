04114340e356f50453313fe03a038a2e3af29b15
Sai Kumar Munagala <saikumar.munagala@gmail.com>
2022-10-25 19:37:11 +0530

    TopNavResponsiveChanges

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -34,18 +34,39 @@ const menuCompany = (<Menu className="main_navigation"
 
 const menuNavigation = () => {
     return <>
-        <Dropdown overlay={menuProducts} trigger={['click']}>
-            <Link to="" className="text-white font_18x padding-r-1_x" onClick={e => e.preventDefault()}>
-                <Space>Products<CaretDownOutlined /></Space> </Link>
-        </Dropdown>
-        <Dropdown overlay={menuEarn} trigger={['click']}>
-            <Link to="" className="text-white font_18x padding-r-1_x" onClick={e => e.preventDefault()}>
-                <Space>Earn<CaretDownOutlined /></Space> </Link>
-        </Dropdown>
-        <Dropdown overlay={menuCompany} trigger={['click']}>
-            <Link to="" className="text-white font_18x padding-r-1_x" onClick={e => e.preventDefault()}>
-                <Space>Company<CaretDownOutlined /></Space> </Link>
-        </Dropdown>
+   
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div className="container-fluid">
+   <div className="width-100 hamburger-icon">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    </div>
+    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#"> <Dropdown overlay={menuProducts} trigger={['click']}>
+            <Link to="" className="text-white font_18x padding-r-1_x nav-item" onClick={e => e.preventDefault()}>
+                <Space className="nav-link dropdown-toggle">Products</Space> </Link>
+        </Dropdown></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"> <Dropdown overlay={menuEarn} trigger={['click']}>
+            <Link to="" className="text-white font_18x padding-r-1_x nav-item" onClick={e => e.preventDefault()}>
+                <Space className="nav-link dropdown-toggle">Earn</Space> </Link>
+        </Dropdown></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"> <Dropdown overlay={menuCompany} trigger={['click']}>
+            <Link to="" className="text-white font_18x padding-r-1_x nav-item" onClick={e => e.preventDefault()}>
+                <Space className="nav-link dropdown-toggle">Company</Space> </Link>
+        </Dropdown></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      
     </>
 }
 
