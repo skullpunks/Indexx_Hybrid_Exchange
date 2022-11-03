ea3d3ece1a87ffcfaf67bc5c3c27c230da1d0f9c
omkarsaiazooca <omkar@azooca.com>
2022-11-03 19:53:27 +0530

    update trade to earn page and iusd+ graph

--- src/components/TradeToEarn/TradeToEarn.tsx
+++ src/components/TradeToEarn/TradeToEarn.tsx
@@ -13,43 +13,45 @@ import no2 from "../../assets/arts/no2.png";
 import no3 from "../../assets/arts/no3.png";
 
 import Footer from '../Footer/Footer';
-// import { decodeJWT, getUserRewards } from '../../services/api';
-
+import { decodeJWT, getUserRewards, getUserWallets } from '../../services/api';
+import { useEffect, useState } from 'react';
 
 const { Text } = Typography;
 
 const TradeToEarn = () => {
   const navigate = useNavigate();
-
+  let [totalBalanceInUSD, settotalBalanceInUSD] = useState(0);
   const navigateUser = (path: any) => {
     window.localStorage.setItem("redirect", window.location.pathname);
     navigate(path);
   }
 
-  /* let totalTokensBalInUSD = 0;
-   let access_token = String(localStorage.getItem("access_token"));
-    let decoded :any= decodeJWT(access_token);*/
+  let totalTokensBalInUSD = 0;
+  let access_token = String(localStorage.getItem("access_token"));
+  let decoded: any = decodeJWT(access_token);
   // onChange =>()= {
   // let userWallets = await getUserWallets(decoded.email);
   // // }
-  // useEffect( async() {
-  // await getUserWallets(decoded.email);
-  // }, []);
+  useEffect(() => {
+    const access_token = String(localStorage.getItem("access_token"));
+    const decoded: any = decodeJWT(access_token);
+    getUserWallets(decoded.email);
+  }, []);
 
   // getUserWallets(decoded.email).then((userWallets) => {
-  //     data = userWallets.data;
+  //     let data = userWallets.data;
   //     userWallets.data.map((wallet: any) => {
   //         totalBalanceInUSD += parseFloat(wallet.coinWalletBalanceInUSD);
   //     })
   //     console.log("data", data);
   // })
 
-  /* getUserRewards(decoded.email).then((userRewards) => {
-     console.log("userRewards", userRewards);
-     totalTokensBalInUSD = userRewards.data.rewardTokenBalanceInUSD;
-     console.log("totalTokensBalInUSD", totalTokensBalInUSD);
-   })
- */
+  getUserRewards(decoded.email).then((userRewards) => {
+    console.log("userRewards", userRewards);
+    settotalBalanceInUSD(userRewards.data.rewardTokenBalanceInUSD);
+    console.log("totalTokensBalInUSD", totalTokensBalInUSD);
+  })
+
   return (
     <>
       <div className='scan-container trade-to-earn flex-direction-column '>
@@ -76,7 +78,7 @@ const TradeToEarn = () => {
                   <Image src={exgcoin} width={120} preview={false}></Image>
                 </div>
                 <h2 className='centered' style={{ marginBottom: 0, color: "#5F5F5F", fontSize: "25px" }}>indexx Exchange (INEX)</h2>
-                <h1 style={{ display: 'flex', marginTop: 0, justifyContent: 'center', alignItems: 'center', opacity: "20%", color: "#5F5F5F", fontSize: "90px" }}>$0</h1>
+                <h1 style={{ display: 'flex', marginTop: 0, justifyContent: 'center', alignItems: 'center', opacity: "20%", color: "#5F5F5F", fontSize: "90px" }}>${Math.floor((totalBalanceInUSD * 100) / 100 )}</h1>
                 <Text className='centered' style={{ marginBottom: 0, color: "#5F5F5F", fontSize: "15px" }}>Minimum limit:100</Text>
                 <br /> <br></br>
                 <Button type="primary" block shape="round" size="large" className="btn_xl" style={{
