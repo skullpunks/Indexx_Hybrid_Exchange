7a4e384cc2a81f3b0c90fcf1067ed173369b4d05
Sai Kumar Munagala <saikumar.munagala@gmail.com>
2022-11-03 22:28:20 +0530

    Wallet Responsive changes

--- src/components/BuySell/BSWallet.tsx
+++ src/components/BuySell/BSWallet.tsx
@@ -12,14 +12,14 @@ import PlainCircle from "../../assets/arts/PlainCircle.svg";
 const BSWallet = () => {
     return (
         <div className='bs_wallet'>
-            <div className='d-flex bs_wallet_top'>
+            {/* <div className='d-flex bs_wallet_top'>
                 <div>
                     <Link to="" className='font_15x text-white' ><CheckCircleOutlined className='padding-r-2x margin-r-0_5x' />Create account</Link>
                 </div>
                 <div>
                     <Link to="" className='font_15x margin-l-2x text-white' ><img src={PlainCircle} alt="PlainCircle" width="15" height="15" className='padding-r-2x' /> <span style={{paddingTop:5}}>Add payment method</span><CaretRightOutlined className='margin-l-0_5x' /></Link>
                 </div>
-            </div>
+            </div> */}
 
             <div className='scan-container d-flex flex-direction-column card large_card orange'>
 
