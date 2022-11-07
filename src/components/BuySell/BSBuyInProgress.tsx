56a5d15b27e9fbf34ba55a0bf2e82237a3635632
Sai Kumar Munagala <talpasaikumar.munagala@gmail.com>
2022-11-07 06:23:52 +0530

    Updated Dollar icon and spacing

--- src/components/BuySell/BSBuyInProgress.tsx
+++ src/components/BuySell/BSBuyInProgress.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import InProgressClock from "../../assets/arts/InProgressClock.svg";
-import SwapArrowIcon from "../../assets/arts/SwapArrowIcon.svg";
+// import SwapArrowIcon from "../../assets/arts/SwapArrowIcon.svg";
 import { Button } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 // import { BSContext, BSContextType } from '../../utils/SwapContext';
@@ -28,12 +28,14 @@ const BSBuyInProgress: React.FC<(Props)> = ({ setScreenName }) => {
         const orderCurr = String(orderCurrency.get("orderCurrency"));
         //setOrderCurrency(orderCurr)
         console.log(orderCurr)
+        console.log();
         setIncurr(orderCurr)
         setInAmt(Number(orderAmount.get("orderAmount")));
         setoutcurr(String(payCurrency.get("payCurrency")));
         setoutAmt(Number(payAmount.get("payAmount")));
     }, [payAmount, payCurrency, orderAmount, orderCurrency])
     console.log(incurr)
+    console.log(inAmt);
 
     // const filteredFromArray = initialTokens.filter(function (obj) {
     //     return obj?.address === BSvalue?.fromToken;
@@ -50,7 +52,7 @@ const BSBuyInProgress: React.FC<(Props)> = ({ setScreenName }) => {
             <div className='card_body text-center'>
                 <img src={InProgressClock} alt="InProgressClock" className='padding-t-2x' />
 
-                <div className="bs_curreny d-flex position-relative ">
+                {/* <div className="bs_curreny d-flex position-relative ">
                     <div className="bs_curreny_left padding-b-2x" style={{ transform: "scale(1)", padding: "35px 20px 0 20px" }}>
                         <span className="font_20x" style={{ lineHeight: 4 }} >$</span>
                         <span placeholder="0" className=" " style={{ fontSize: 60 }} >{inAmt}</span>
@@ -58,8 +60,8 @@ const BSBuyInProgress: React.FC<(Props)> = ({ setScreenName }) => {
                     <div className='swap_Arrow_icon' style={{ position: "absolute", right: "4px", top: "6%" }}>
                         <img src={SwapArrowIcon} className="hover_icon" alt="ddd" />
                     </div>
-                </div>
-                <div className="bs_curreny_left padding-b-2x" style={{ transform: "scale(1)", paddingBottom: "50px", paddingTop: 0 }}>
+                </div> */}
+                <div className="bs_curreny_left p-3 " style={{ transform: "scale(1)", paddingBottom: "50px", paddingTop: 0, alignItems: "baseline" }}>
 
                     <span placeholder="0" className="font_20x " style={{ fontSize: 60 }} >{Math.floor(outAmt * 100) / 100}</span>
                     <span className="font_20x" style={{
@@ -68,7 +70,7 @@ const BSBuyInProgress: React.FC<(Props)> = ({ setScreenName }) => {
 
                 </div>
 
-                <div className='font_20x padding-b-2x'>Your convert order is being processed.</div>
+                <div className='font_20x padding-b-2x'>Your buy order is being processed.A confirmation email will be sent once the order is complete.</div>
                 <Button type="primary" className="atn-btn atn-btn-round margin-b-1x" block onClick={() => navigate("/indexx-exchange/buy-sell/wallet")}> Go to Wallet</Button>
                 <Link className="font_15x bs_link text-center d-block padding-t-3x" to="/indexx-exchange/buy-sell?type=buy" >New Buy</Link>
             </div>
