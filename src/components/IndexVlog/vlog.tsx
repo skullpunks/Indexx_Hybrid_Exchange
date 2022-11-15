28667bf0a111c478418ea36328ac4b48101843e9
Kamal Farooqi <76092669+kamalaqeel@users.noreply.github.com>
2022-11-15 20:22:11 +0500

    added vlog

--- src/components/IndexVlog/vlog.tsx
+++ src/components/IndexVlog/vlog.tsx
@@ -1,26 +1,61 @@
-import React, { useEffect, useState } from "react";
-import Footer from "../Footer/Footer";
+
 import { Image } from "antd";
-import camera from '../../assets/arts/camera.png'
+import camera from "../../assets/arts/camera.png";
+import you1 from "../../assets/arts/youtube1.webp";
+
 const vlog = () => {
+
   return (
     <>
       <div className="scan-container trade-to-earn flex-direction-column ">
         <div className="row">
-            <div className="col-1" style={{justifyContent:'normal'}}>
-            <Image src={camera} className="float-left " preview={false} style={{width:80,height:52,paddingLeft:10,marginTop:20}}>Vlog</Image>
-            </div>
-            <div className="col">
-              <p className="opacity-80" style={{fontWeight:500, fontSize:60}}>VLOG</p>
-             
-            </div>
-
+          <div className="col" style={{ justifyContent: "normal" }}>
+            <Image
+              src={camera}
+              className="float-left "
+              preview={false}
+              style={{ width: 80, height: 52, marginTop: -20 }}
+            >
+              Vlog
+            </Image>
+            <span
+              className="opacity-80 p-100"
+              style={{
+                paddingLeft: 30,
+                marginTop: 100,
+                paddingTop: 100,
+                fontWeight: 500,
+                fontSize: 60,
+              }}
+            >
+              VLOG
+            </span>
+          </div>
         </div>
-        
         <div className="row">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/kfbl3WqJy3E" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+          <div className="col">
+          <a href="https://www.youtube.com/watch?v=kfbl3WqJy3E&ab_channel=IndexxAi">
+         <Image
+              src={you1}
+              className="align-left float-left"
+              preview={false}
+              style={{  width:587, height:330,marginTop:50 }}
+              
+            >
+              Vlog
+            </Image>
+            <p className="opacity-75" style={{fontSize:40,fontWeight:400,marginLeft:30}}> Indexx Trade to Earn launch</p>
+
+
+        </a>
         </div>
-      </div>
+          </div>
+       
+  
+     
+          
+        </div>
+   
       {/* <Footer></Footer> */}
     </>
   );
