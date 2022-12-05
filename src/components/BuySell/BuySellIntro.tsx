cc0b3eb8cdc11c7c4066eb1eb751d63f60544f78
Kamal Farooqi <76092669+kamalaqeel@users.noreply.github.com>
2022-12-05 16:26:05 +0500

    added warning and modal

--- src/components/BuySell/BuySellIntro.tsx
+++ src/components/BuySell/BuySellIntro.tsx
@@ -5,6 +5,12 @@ import BSSellIntro from "./BSSellIntro";
 import BuyContent from "./BuyContent";
 import { BSContext, BSContextType } from '../../utils/SwapContext';
 import { useNavigate, useSearchParams } from "react-router-dom";
+import lockedimage from "../../assets/arts/locked.png"
+import { Button, Checkbox, Form, Input, Modal, notification ,Image} from 'antd';
+import { CheckCircleFilled, CloseCircleFilled } from '@ant-design/icons';
+import { geolocationData } from '../../services/api';
+import { useState, useEffect } from 'react';
+import "./BuySellIntro.css"
 
 interface Props {
     setScreenName: (value: string | ((prevVar: string) => string)) => void;
@@ -17,6 +23,45 @@ const filteredArray = (items: any, keyName: any, key: any) => {
 }
 
 const BuySellIntro: React.FC<(Props)> = ({ setScreenName }) => {
+
+    const [ip, setIP] = useState('');
+    const [country, setCountry] = useState('');
+    const [countryCode, setCountryCode] = useState('');
+    const [isTransferModalVisible, setIsTransferModalVisible] = useState(true);
+    const handleTransferOk = () => {
+        localStorage.setItem('userIp', ip);
+        setIsTransferModalVisible(false);
+    };
+
+    const handleTransferCancel = () => {
+        setIsTransferModalVisible(false);
+    };
+
+    const handleOk = () => {
+        // setLoading(true);
+    };
+
+    const [loadings, setLoadings] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        geolocationData().then((res: any) => {
+       // axios.get('https://geolocation-db.com/json/').then((res: any) => {
+            console.log(res.data);
+            setIP(res.data.IPv4);
+            setCountry(res.data.country_name);
+            setCountryCode(res.data.country_code);
+            if (res.data.country_code === 'US') {
+                setIsTransferModalVisible(true);
+            }
+        })
+    }, []);
+
+
+       
+
+
+
     const userId = localStorage.getItem("user");
     const navigate = useNavigate();
     const { BSvalue, setBSvalue } = React.useContext(BSContext) as BSContextType;
@@ -27,6 +72,10 @@ const BuySellIntro: React.FC<(Props)> = ({ setScreenName }) => {
     ];
     const callback = function (key: any) {
         // console.log(key);
+        if (key ==="2" && (country === "United States" || countryCode === "US")){
+            setIsTransferModalVisible(true)
+        }
+
         const filteredFromArray = items.filter(function (obj) {
             return obj?.key === key;
         });
@@ -44,10 +93,44 @@ const BuySellIntro: React.FC<(Props)> = ({ setScreenName }) => {
         let fileteredArrayWithItem = filteredArray(items, "value", orderType);
         activeKey = fileteredArrayWithItem[0].key;
     }
-
+    
+ 
+      
     return (<div className="bs_container card">
         {(userId) ?
+            <div>
+        <Modal centered={true}  visible={isTransferModalVisible} onOk={handleTransferOk} onCancel={handleTransferCancel} width={670} maskClosable={false} 
+                            footer={[
+
+                                <Button
+                                    className="btn text-white  btn-primary"
+                                    danger
+                                    size='large'
+                                    href="https://dex.indexx.ai"
+                                    style={{marginBottom:20,width:"100%"}}
+                                    type="primary"
+                                    onClick={handleOk}>
+                                    
+                                    Go To Decentralized
+                                </Button>,
+
+                                <Button
+                                    className="center"
+                                    type="link"
+                                    onClick={handleTransferCancel}>
+                                    Cancel
+                                </Button>,
+
+                            ]}>
+                                
+                            <div className="align-center text-center">
+                                <Image preview={false} src={lockedimage}></Image>
+                                <p className="text-center" style={{fontSize:30,fontWeight:400,}}>Service Notice</p>
+                                <p>Your IP address indicates that you’re attempting to access our services from the USA. As per our Terms and Use, we’re unable to provide services to users from the region. Instead, please register on our partner platform dedicated to American customers. </p>
+                            </div>
+                        </Modal>
             <Tabs defaultActiveKey={activeKey} items={items} className="bs_tab_item" onChange={callback} />
+            </div>
             :
             <>
                 <div className="bs_container_header">
