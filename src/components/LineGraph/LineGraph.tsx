ce04078e90df1857ea301a58be5b3c5f0e68ebbf
stutidpatel <stuti.patel@protonmail.com>
2023-04-10 12:02:25 +0530

    y axis

--- src/components/LineGraph/LineGraph.tsx
+++ src/components/LineGraph/LineGraph.tsx
@@ -222,7 +222,7 @@ const LineGraph = (props: any) => {
               padding={{ top: 20 }}
               tick={{ fill: '#5f5f5f' }}
               domain={['auto', 'auto']}
-              hide={true}
+              hide={false}
             />
             {/* <Tooltip position={{ y: -10 }}
               labelFormatter={dateFormatter3}
