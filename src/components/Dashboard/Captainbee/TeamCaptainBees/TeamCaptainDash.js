950032d9f6d817b13dd2fad6788a3856c40e318d
omkarsunku <sunkuomkarsai@gmail.com>
2023-10-14 10:13:58 +0530

    add sample empty renders

--- src/components/Dashboard/Captainbee/TeamCaptainBees/TeamCaptainDash.js
+++ src/components/Dashboard/Captainbee/TeamCaptainBees/TeamCaptainDash.js
@@ -144,6 +144,259 @@ const TeamCaptainDash = () => {
     openNotificationWithIcon('success', 'Copied Successfully!');
   };
 
+  // Define a function to render a single Honey Bee box.
+  const renderHoneyBeeBox = (item) => (
+    <Grid item xs={1} sm={6} md={3} >
+      <div className="d-flex flex-direction-column">
+        <div className="d-flex align-items-center">
+          <div
+            style={{
+              width: '80px',
+              height: '80px',
+              backgroundImage: `url(${frame})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
+              position: 'relative',
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+              // border:"none"
+            }}
+          >
+            <div
+              className="bee-hexagon"
+              style={{ marginBottom: '7px' }}
+            >
+              <img
+                alt=""
+                src={item?.profilePic === undefined ? dummy : item?.profilePic}
+                width={'63px'}
+                height={'66px'}
+                ml={'-6px'}
+                border={'none'}
+              />
+            </div>
+          </div>
+
+          <Box
+            className=" d-flex justify-content-center"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "baseline",
+              backgroundColor: 'transparent',
+              border: "1.5px solid #E1E1E1",
+              height: '50px',
+              marginLeft: '-35px',
+              pl: 4,
+              width: '211px',
+              transition: "0.3s ease-in-out",
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+              },
+            }}
+          >
+            <div className="font_15x d-flex align-items-center">
+              CaptainBee
+            </div>
+            <div className="font_15x d-flex align-items-center">
+              {item.username}
+            </div>
+          </Box>
+        </div>
+        <div
+          className="d-flex align-items-center justify-content-start mt-1"
+          style={{ marginLeft: '14px' }}
+        >
+          <Button
+            variant="outlined"
+            href={`/indexx-exchange/buy-sell/for-honeybee/${item.username}`}
+            // onClick={handleSubmit}
+            disableTouchRipple
+            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
+            sx={{
+              borderColor: '#FFB300',
+              borderRadius: '2px',
+              color: 'var(--body_color)',
+              width: '120px',
+              height: '32px',
+              textTransform: 'none',
+              fontSize: '10px',
+              boxShadow: 'none',
+              transition: "0.3s ease-in-out",
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+                boxShadow: 'none',
+                color: 'var(--body_color)',
+                // color: '#282828',
+              },
+            }}
+          >
+            Exchange
+          </Button>
+          <Button
+            variant="outlined"
+            // onClick={handleSubmit}
+            href={`/indexx-exchange/dashboard/capt-mybees/${item.username}/1/CaptainBee`}
+            disableTouchRipple
+            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
+            sx={{
+              borderColor: '#FFB300',
+              borderRadius: '2px',
+              color: 'var(--body_color)',
+              // color: '#282828',
+              width: '120px',
+              height: '32px',
+              textTransform: 'none',
+              fontSize: '10px',
+              boxShadow: 'none',
+              transition: "0.3s ease-in-out",
+              ml: 0.3,
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+                // color: '#282828',
+                color: 'var(--body_color)',
+                boxShadow: 'none',
+              },
+            }}
+          >
+            DashBoard
+          </Button>
+        </div>
+      </div>
+    </Grid>
+  );
+
+  // Calculate the number of empty boxes to render.
+  const availableBeesCount = staticsData?.captainBeeRegisteredRequiredData?.length || 0;
+  const emptyBoxesCount = Math.max(6 - availableBeesCount, 0);
+
+  // Define a function to render an empty Honey Bee box with the same styling as available users.
+  const renderEmptyHoneyBeeBox = (index) => (
+    <Grid item xs={1} sm={6} md={3}>
+      <div className="d-flex flex-direction-column">
+        <div className="d-flex align-items-center">
+          <div
+            style={{
+              width: '80px',
+              height: '80px',
+              backgroundImage: `url(${frame})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
+              position: 'relative',
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}
+          >
+            <div className="bee-hexagon" style={{ marginBottom: '7px' }}>
+              {/* You can add any text or icons here to indicate an empty box */}
+            </div>
+          </div>
+
+          <Box
+            className=" d-flex justify-content-center"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "baseline",
+              backgroundColor: 'transparent',
+              border: "1.5px solid #E1E1E1",
+              height: '50px',
+              marginLeft: '-35px',
+              pl: 4,
+              width: '211px',
+              transition: "0.3s ease-in-out",
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+              },
+            }}
+          >
+            <div className="font_15x d-flex align-items-center">
+              Captain Bee
+            </div>
+            <div className="font_15x d-flex align-items-center">
+              {/* You can add any placeholder text here */}
+            </div>
+          </Box>
+        </div>
+        <div
+          className="d-flex align-items-center justify-content-start mt-1"
+          style={{ marginLeft: '14px' }}
+        >
+          <Button
+            variant="outlined"
+            // onClick={handleSubmit}
+            disableTouchRipple
+            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
+            sx={{
+              // borderColor: '#FFB300',
+              // borderRadius: '2px',
+              // color: 'var(--body_color)',
+              borderColor: '#E1E1E1', // Grey border color
+              borderRadius: '2px',
+              color: '#E1E1E1', // Grey text color
+              width: '120px',
+              height: '32px',
+              textTransform: 'none',
+              fontSize: '10px',
+              boxShadow: 'none',
+              transition: "0.3s ease-in-out",
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+                boxShadow: 'none',
+                color: 'var(--body_color)',
+                // color: '#282828',
+              },
+            }}
+          >
+            Exchange
+          </Button>
+          <Button
+            variant="outlined"
+            // onClick={handleSubmit}
+            disableTouchRipple
+            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
+            sx={{
+              borderColor: '#E1E1E1', // Grey border color
+              borderRadius: '2px',
+              color: '#E1E1E1', // Grey text color
+              // color: '#282828',
+              width: '120px',
+              height: '32px',
+              textTransform: 'none',
+              fontSize: '10px',
+              boxShadow: 'none',
+              transition: "0.3s ease-in-out",
+              ml: 0.3,
+              '&:hover': {
+                backgroundColor: '#FFB300',
+                borderColor: '#FFB300',
+                // color: '#282828',
+                color: 'var(--body_color)',
+                boxShadow: 'none',
+              },
+            }}
+          >
+            DashBoard
+          </Button>
+        </div>
+      </div>
+    </Grid>
+  );
 
 
   return (
@@ -174,142 +427,12 @@ const TeamCaptainDash = () => {
                 maxWidth={"1150px"}
                 rowSpacing={12}
               >
-                {staticsData?.captainBeeRegisteredRequiredData?.length > 0 ?
-                  (staticsData?.captainBeeRegisteredRequiredData?.map((item) => (
-                    <Grid item xs={1} sm={6} md={3} >
-                      <div className="d-flex flex-direction-column">
-                        <div className="d-flex align-items-center">
-                          <div
-                            style={{
-                              width: '80px',
-                              height: '80px',
-                              backgroundImage: `url(${frame})`,
-                              backgroundRepeat: 'no-repeat',
-                              backgroundSize: 'contain',
-                              backgroundPosition: 'center',
-                              position: 'relative',
-                              cursor: 'pointer',
-                              display: 'flex',
-                              justifyContent: 'center',
-                              alignItems: 'center',
-                              alignSelf: 'center',
-                              // border:"none"
-                            }}
-                          >
-                            <div
-                              className="bee-hexagon"
-                              style={{ marginBottom: '7px' }}
-                            >
-                              <img
-                                alt=""
-                                src={item?.profilePic === undefined ? dummy : item?.profilePic}
-                                width={'63px'}
-                                height={'66px'}
-                                ml={'-6px'}
-                                border={'none'}
-                              />
-                            </div>
-                          </div>
 
-                          <Box
-                            className=" d-flex justify-content-center"
-                            sx={{
-                              display: "flex",
-                              flexDirection: "column",
-                              justifyContent: "center",
-                              alignItems: "baseline",
-                              backgroundColor: 'transparent',
-                              border: "1.5px solid #E1E1E1",
-                              height: '50px',
-                              marginLeft: '-35px',
-                              pl: 4,
-                              width: '211px',
-                              transition: "0.3s ease-in-out",
-                              '&:hover': {
-                                backgroundColor: '#FFB300',
-                                borderColor: '#FFB300',
-                              },
-                            }}
-                          >
-                            <div className="font_15x d-flex align-items-center">
-                              Captain Bee
-                            </div>
-                            <div className="font_15x d-flex align-items-center">
-                              {item.username}
-                            </div>
-                          </Box>
-                        </div>
-                        <div
-                          className="d-flex align-items-center justify-content-start mt-1"
-                          style={{ marginLeft: '14px' }}
-                        >
-                          <Button
-                            variant="outlined"
-                            href={`/indexx-exchange/buy-sell/for-honeybee/${item.username}`}
-                            // onClick={handleSubmit}
-                            disableTouchRipple
-                            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
-                            sx={{
-                              borderColor: '#FFB300',
-                              borderRadius: '2px',
-                              color: 'var(--body_color)',
-                              width: '120px',
-                              height: '32px',
-                              textTransform: 'none',
-                              fontSize: '10px',
-                              boxShadow: 'none',
-                              transition: "0.3s ease-in-out",
-                              '&:hover': {
-                                backgroundColor: '#FFB300',
-                                borderColor: '#FFB300',
-                                boxShadow: 'none',
-                                color: 'var(--body_color)',
-                                // color: '#282828',
-                              },
-                            }}
-                          >
-                            Exchange
-                          </Button>
-                          <Button
-                            variant="outlined"
-                            // onClick={handleSubmit}
-                            href={`/indexx-exchange/dashboard/capt-mybees/${item.username}/1/CaptainBee`}
-                            disableTouchRipple
-                            // disabled={!isChecked || !isChecked2 || !frontFile || !backFile || !photoIdFile} // Disable if frontFile is null
-                            sx={{
-                              borderColor: '#FFB300',
-                              borderRadius: '2px',
-                              color: 'var(--body_color)',
-                              // color: '#282828',
-                              width: '120px',
-                              height: '32px',
-                              textTransform: 'none',
-                              fontSize: '10px',
-                              boxShadow: 'none',
-                              transition: "0.3s ease-in-out",
-                              ml: 0.3,
-                              '&:hover': {
-                                backgroundColor: '#FFB300',
-                                borderColor: '#FFB300',
-                                // color: '#282828',
-                                color: 'var(--body_color)',
-                                boxShadow: 'none',
-                              },
-                            }}
-                          >
-                            DashBoard
-                          </Button>
-                        </div>
-                      </div>
-                    </Grid>
-                  ))) :
-                  <>
-                    <div className="d-flex flex-direction-column justify-content-center" style={{ paddingTop: "140px" }}>
-                      <div className="d-flex align-items-center justify-content-center">
-                        <h2 style={{ color: "var( --body_color)" }}> No Captain Bees Available to list </h2>
-                      </div>
-                    </div>
-                  </>}
+                {staticsData?.captainBeeRegisteredRequiredData?.map(renderHoneyBeeBox)}
+                {/* Render empty gray boxes to fill the remaining slots first */}
+                {Array.from({ length: emptyBoxesCount }, (_, index) => (
+                  renderEmptyHoneyBeeBox(index)
+                ))}
               </Grid>
 
             </Box>
