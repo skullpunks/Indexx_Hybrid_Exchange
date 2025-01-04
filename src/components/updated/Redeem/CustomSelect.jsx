006df2ee44327147cd04f42cc4b15aee986b9631
omkarsunku <sunkuomkarsai@gmail.com>
2025-01-04 21:42:04 +0530

    Added to fixed(2)

--- src/components/updated/Redeem/CustomSelect.jsx
+++ src/components/updated/Redeem/CustomSelect.jsx
@@ -154,7 +154,7 @@ export default function CustomSelectBox({
                 value={card.voucher}
                 disabled={card.isUsed}
               >
-                {`${card.voucher} - ${card.amount} ${card.type}(Amount in USD: ${card.type === 'USD' ? card.amount: card.amount * card.price}, email:${card?.assignedToUser ? card?.assignedToUser : 'NA'})`}
+                {`${card.voucher} - ${card.amount} ${card.type}(Amount in USD: ${card.type === 'USD' ? card.amount: Number(card.amount * card.price).toFixed(2)}, email:${card?.assignedToUser ? card?.assignedToUser : 'NA'})`}
               </MenuItem>
             ))
           : !isCurrency
