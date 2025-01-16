4a48f2b62712070a3af3bc10ce57b2f3ba17d2a3
omkarsunku <sunkuomkarsai@gmail.com>
2025-01-16 22:15:23 +0530

    updated the notes Jan_2025($5,000), Jan_2025($6,000)

--- src/components/updated/Assets/CoinTable.jsx
+++ src/components/updated/Assets/CoinTable.jsx
@@ -563,7 +563,7 @@ export default function EnhancedTable({
       'Wave',
       'Surge',
     ];
-    const managerMappings = ['Omkar', 'Kashir', 'Issa' , 'Jan-2025', 'Feb-2025', 'Mar-2025', 'Gabe' , 'Jan-2025($5,000)', 'Jan-2025($6,000)'];
+    const managerMappings = ['Omkar', 'Kashir', 'Issa' , 'Jan-2025', 'Feb-2025', 'Mar-2025', 'Gabe' , 'Jan_2025($5,000)', 'Jan_2025($6,000)'];
 
     // Find the crypto type and manager from the note
     const cryptoType =
